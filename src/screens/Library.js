e1423152085e43c9bb02f96910b77572a38050f7
jozanda <jossanda17@gmail.com>
2024-04-01 14:36:01 +0700

    add search and library screen

--- src/screens/Library.js
+++ src/screens/Library.js
@@ -1,26 +1,167 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Image, StyleSheet, ScrollView} from 'react-native';
 import NavBar from '../components/NavBar';
+import HeaderComponent from '../components/HeaderComponent';
+import HorizontalLine from '../components/HorizontalLine';
+
+const FAV = [
+  {
+    image: require('../image/favorite.png'),
+    title: 'Favorite',
+    artist: 'Playlist . 13 songs',
+  },
+];
+
+const MUSIC = [
+  {
+    image: require('../image/yummy.png'),
+    title: 'Yummy',
+    artist: 'Justin Bieber',
+  },
+  {
+    image: require('../image/cleanbandit.png'),
+    title: 'Symphony',
+    artist: 'Clean Bandit',
+  },
+  {
+    image: require('../image/dualipa.png'),
+    title: 'Levitating',
+    artist: 'Dua Lipa, DaBaby',
+  },
+];
+
+const PLAYLIST = [
+  {
+    image: require('../image/playlist.png'),
+    title: 'Autums variations',
+    artist: 'ED Sheeran',
+  },
+];
 
 class Library extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  renderFavItems() {
+    return FAV.map((item, index) => (
+      <View key={index}>
+        <View style={styles.musicItem}>
+          <Image source={item.image} style={styles.musicImage} />
+          <View style={styles.musicDetails}>
+            <Text style={styles.musicTitle}>{item.title}</Text>
+            <Text style={styles.musicArtist}>{item.artist}</Text>
+          </View>
+        </View>
+      </View>
+    ));
+  }
+
+  renderMusicItems() {
+    return MUSIC.map((item, index) => (
+      <View key={index}>
+        <View style={styles.musicItem}>
+          <Image source={item.image} style={styles.musicImage} />
+          <View style={styles.musicDetails}>
+            <Text style={styles.musicTitle}>{item.title}</Text>
+            <Text style={styles.musicArtist}>{item.artist}</Text>
+          </View>
+        </View>
+      </View>
+    ));
+  }
+
+  renderPlaylistItems() {
+    return PLAYLIST.map((item, index) => (
+      <View key={index}>
+        <View style={styles.musicItem}>
+          <Image source={item.image} style={styles.musicImage} />
+          <View style={styles.musicDetails}>
+            <Text style={styles.musicTitle}>{item.title}</Text>
+            <Text style={styles.musicArtist}>{item.artist}</Text>
+          </View>
+        </View>
+      </View>
+    ));
+  }
+
   render() {
     return (
-      <View>
-        {/* Height nde iki iso diilangi */}
-        <Text style={{height: 100}}>Library</Text>
-        {/* <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-          <Text>Back</Text>
-        </TouchableOpacity> */}
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.header}>
+            <HeaderComponent title={'Library'} fontSize={40} />
+          </View>
 
-      <NavBar navigation={this.props.navigation} />
+          <View style={styles.containerFav}>
+            <HeaderComponent title={'Recent Played'} fontSize={25} />
+            <HorizontalLine width={'100%'} alignSelf={'center'} />
+            {this.renderFavItems()}
+          </View>
+          <HorizontalLine width={'80%'} alignSelf={'center'} />
+          <View style={styles.containerMusic}>
+            <HeaderComponent title={'Songs'} fontSize={25} />
+            {this.renderMusicItems()}
+          </View>
+          <HorizontalLine width={'80%'} alignSelf={'center'} />
+          <View style={styles.containerFav}>
+            <HeaderComponent title={'Playlist'} fontSize={25} />
+            {this.renderPlaylistItems()}
+          </View>
+        </ScrollView>
+       <NavBar navigation={this.props.navigation} />
       </View>
     );
   }
 }
+
+const styles =StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 20,
+  },
+
+  header: {
+    marginBottom:0,
+  },
+
+  containerFav:{
+    marginTop: 0,
+    marginBottom: 10,
+  },
+
+  containerMusic: {
+    marginBottom: 10,
+  },
+
+  musicItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  musicImage: {
+    borderRadius: 10,
+    width:70,
+    height:70,
+  },
+
+  musicDetails: {
+    marginLeft: 20,
+  },
+
+  musicTitle: {
+    color: 'black',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  musicArtist: {
+    color: 'black',
+    fontSize: 15,
+  },
+})
+
 
 export default Library;
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,26 +1,129 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, ScrollView, TextInput, Image} from 'react-native';
+import HeaderComponent from '../components/HeaderComponent';
 import NavBar from '../components/NavBar';
+
+const MUSIC = [
+  {
+    image: require('../image/anniemarrie.png'),
+    title: '2002',
+    artist: 'Annie-Marrie',
+  },
+  {
+    image: require('../image/cleanbandit.png'),
+    title: 'Symphony',
+    artist: 'Clean Bandit',
+  },
+  {
+    image: require('../image/dualipa.png'),
+    title: 'Levitating',
+    artist: 'Dua Lipa, DaBaby',
+  },
+];
 
 class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchText: '',
+    };
   }
-  render() {
-    return (
-      <View>
-        {/* Height nde iki iso diilangi */}
-        <Text style={{height: 100}}>Search</Text>
-        {/* <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-          <Text>Back</Text>
-        </TouchableOpacity> */}
 
-      <NavBar navigation={this.props.navigation} />
+  renderMusicItems() {
+    return MUSIC.map((item, index) => (
+      <View key={index}>
+        <View style={styles.musicItem}>
+          <Image source={item.image} style={styles.musicImage} />
+          <View style={styles.musicDetails}>
+            <Text style={styles.musicTitle}>{item.title}</Text>
+            <Text style={styles.musicArtist}>{item.artist}</Text>
+          </View>
+        </View>
       </View>
+    ));
+  }
+
+  render() {
+    const { searchText } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+
+          <View style={styles.header}>
+            <HeaderComponent title={'Search'} fontSize={40} />
+          </View>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Search artist or song" // Menambahkan placeholder
+            placeholderTextColor="#dcdcdc"
+            onChangeText={text => this.setState({ searchText: text })} // Mengubah state searchText saat teks berubah
+            value={searchText} // Mengatur nilai TextInput dari state
+          />
+
+          <View style={styles.containerMusic}>
+            <HeaderComponent title={'Recent Search'} fontSize={25} />
+            {this.renderMusicItems()}
+          </View>
+        </ScrollView>
+        <NavBar navigation={this.props.navigation} />
+      </View>
+      
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 20,
+  },
+
+  header: {
+    marginBottom: 10,
+  },
+
+  input: {
+    backgroundColor:"grey",
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius:10,
+    borderColor:"grey",
+  },
+
+  containerMusic: {
+    marginBottom: 55,
+  },
+
+  musicItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  musicImage: {
+    borderRadius: 10,
+    width:70,
+    height:70,
+  },
+
+  musicDetails: {
+    marginLeft: 20,
+  },
+
+  musicTitle: {
+    color: 'black',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  musicArtist: {
+    color: 'black',
+    fontSize: 15,
+  },
+
+})
 export default Search;
